c399ec8e968e3dbef03411c411dfdfbe46bf025e
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-07-03 17:22:29 +0500

    filled validation

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const SignupPage = () => {
   const [user, setUser] = useState({
@@ -13,7 +13,26 @@ const SignupPage = () => {
 
   const signupHandler = async () => {
     // Handle sign up logic here
+    try {
+    } catch (error) {
+    } finally {
+    }
   };
+
+  const router = useRouter();
+
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
@@ -59,7 +78,7 @@ const SignupPage = () => {
             className="border-2 border-gray-500 rounded-lg px-4 py-2 bg-gray-500 text-white font-bold hover:bg-gray-700 transition duration-300"
             onClick={signupHandler}
           >
-            Signup here
+            {buttonDisabled ? "Fill all fields" : "Signup"}
           </button>
           <Link href="/login">
             <p className="text-blue-700 text-center border-gray-500 rounded-lg">
