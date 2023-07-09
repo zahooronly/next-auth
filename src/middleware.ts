3c175eb5c13687f2658c5a4296cf67c9bc87db6f
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-07-09 21:21:44 +0500

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-// // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
     const path=request.nextUrl.pathname;
     const isPublicPath=path=="/login"||path=="/signup";
@@ -9,11 +8,9 @@ export function middleware(request: NextRequest) {
     const token=request.cookies.get("token")?.value||"";
     if (isPublicPath && token){
         return NextResponse.redirect(new URL("/profile",request.nextUrl));
-        // return NextResponse.redirect('/profile', { status: 302 },request.nextUrl);
     }
     if (!isPublicPath && !token){
         return NextResponse.redirect(new URL("/login",request.nextUrl));
-        // return NextResponse.redirect(new URL('/login', request.url));
     }
 }
 
@@ -21,7 +18,7 @@ export const config = {
     matcher: [
     "/",
     "/profile",
-    "/profile/:id",
+    // "/profile/:id",
     "/login",
     "/signup",
     ]
